3595dfa1be785cdfab02fc5ea4075f7eef4c4d1f
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2018-08-06 18:24:31 -0400

    Invert test case to fail when MatchError is thrown
    I realized that the test case asserted that the MatchError _would_ be
    thrown, which is misleading as that would show a green test (which
    proves my issue).
    
    I have inverted the test so that it fails when MatchError is thrown, and
    passes when it is not thrown.

--- test/wallaby_bug_demo_web/match_error_feature_test.exs
+++ test/wallaby_bug_demo_web/match_error_feature_test.exs
@@ -1,17 +1,22 @@
 defmodule WallabyBugDemoWeb.MatchErrorFeatureTest do
   use WallabyBugDemoWeb.FeatureCase
 
-  test "should raise match error", %{session: session} do
-    assert_raise(MatchError, fn ->
+  test "shouldn't raise match error", %{session: session} do
+    try do
       session
       |> visit("/")
       |> execute_script("""
       const response = await fetch("www.google.com")
-        .then(r => r.text())
-        .then(t => t);
+      .then(r => r.text())
+      .then(t => t);
 
       return response;
       """)
-    end)
+
+      assert true
+    rescue
+      MatchError ->
+        assert false
+    end
   end
 end
